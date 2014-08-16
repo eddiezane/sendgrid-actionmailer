690dd2fd3afaa2f324c0ad5636e6c442803c9507
Eddie Zaneski <eddiezane@gmail.com>
2014-08-15 21:56:08 -0400

    Add railtie

--- lib/sendgrid-rails.rb
+++ lib/sendgrid-rails.rb
@@ -1,15 +1,26 @@
 require 'sendgrid-rails/version'
+require 'sendgrid-rails/railtie' if defined? Rails
 
 require 'sendgrid-ruby'
 
 module SendGridRails
   class DeliveryMethod
-    def initialize(params = {})
-      @client = SendGrid::Client.new(params)
+    def initialize(params)
+      @client = SendGrid::Client.new do |c|
+        c.api_user = ENV['SENDGRID_USERNAME']
+        c.api_key = ENV['SENDGRID_PASSWORD']
+      end
     end
 
     def deliver!(mail)
-      @client.send(mail)
+      email = SendGrid::Mail.new do |m|
+        m.to = mail[:to]
+        m.from = mail[:from]
+        m.subject = mail[:subject]
+        m.text = mail[:text]
+        m.html = mail[:html]
+      end
+      @client.send(email)
     end
   end
 end
---
+++ lib/sendgrid-rails/railtie.rb
@@ -0,0 +1,9 @@
+module SendGridRails
+  class Railtie < Rails::Railtie
+    initializer 'sendgrid_rails.add_delivery_method' do
+      ActiveSupport.on_load :action_mailer do
+        ActionMailer::Base.add_delivery_method :sendgrid_rails, SendGridRails::DeliveryMethod
+      end
+    end
+  end
+end
