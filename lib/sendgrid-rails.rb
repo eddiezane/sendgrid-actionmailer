3b8a68182a10454ceb9681fb8488396f099799d8
Eddie Zaneski <eddiezane@gmail.com>
2014-08-18 17:48:47 -0700

    Hooking into the right place

--- lib/sendgrid-rails.rb
+++ lib/sendgrid-rails.rb
@@ -7,18 +7,18 @@ module SendGridRails
   class DeliveryMethod
     def initialize(params)
       @client = SendGrid::Client.new do |c|
-        c.api_user = Rails.application.config.sendgrid_rails_settings[:api_user]
-        c.api_key = Rails.application.config.sendgrid_rails_settings[:api_key]
+        c.api_user = params[:api_user]
+        c.api_key  = params[:api_key]
       end
     end
 
     def deliver!(mail)
       email = SendGrid::Mail.new do |m|
-        m.to = mail[:to]
-        m.from = mail[:from]
+        m.to      = mail[:to]
+        m.from    = mail[:from]
         m.subject = mail[:subject]
-        m.text = mail[:text]
-        m.html = mail[:html]
+        m.text    = mail[:text]
+        m.html    = mail[:html]
       end
       @client.send(email)
     end
--- lib/sendgrid-rails/railtie.rb
+++ lib/sendgrid-rails/railtie.rb
@@ -1,9 +1,7 @@
 module SendGridRails
   class Railtie < Rails::Railtie
-    initializer 'sendgrid_rails.add_delivery_method' do
-      ActiveSupport.on_load(:action_mailer) do
-        ActionMailer::Base.add_delivery_method(:sendgrid_rails, SendGridRails::DeliveryMethod)
-      end
+    initializer 'sendgrid_rails.add_delivery_method', before: 'action_mailer.set_configs' do
+      ActionMailer::Base.add_delivery_method(:sendgrid_rails, SendGridRails::DeliveryMethod)
     end
   end
 end
