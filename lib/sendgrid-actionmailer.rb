1d3a5fde68e9708165f38ea5f4a787e34aaaee91
Eddie Zaneski <eddiezane@gmail.com>
2014-10-25 14:27:30 -0700

    Handle mime

--- lib/sendgrid-actionmailer.rb
+++ lib/sendgrid-actionmailer.rb
@@ -17,10 +17,37 @@ module SendGridActionMailer
         m.to      = mail[:to]
         m.from    = mail[:from]
         m.subject = mail[:subject]
-        m.text    = mail[:text]
-        m.html    = mail[:html]
       end
-      @client.send(email)
+
+      # TODO: This is pretty ugly
+      case mail.mime_type
+      when 'text/plain'
+        # Text
+        email.text = mail.body.decoded
+      when 'text/html'
+        # HTML
+        email.html = mail.body.decoded
+      when 'multipart/alternative'
+        # Text and HTML
+        email.text = mail.text_part.decoded
+        email.html = mail.html_part.decoded
+      when 'multipart/mixed'
+        # Text and/or HTML and Attachment
+        if mail.text_part.nil?
+          email.html = mail.html_part.decoded
+        elsif mail.html_part.nil?
+          email.text = mail.text_part.decoded
+        else
+          email.text = mail.text_part.decoded
+          email.html = mail.html_part.decoded
+        end
+
+        mail.attachments.each do |a|
+        end
+      end
     end
+
+    # TODO:
+    # @client.send(email)
   end
 end
