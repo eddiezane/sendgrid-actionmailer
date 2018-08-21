413db5685f782547bf7db0f019d1749660d0fc10
Ty Rauber <tyrauber@mac.com>
2018-08-21 12:19:37 -0700

    Backwards compatibility with mail 2.5.2 and 2.6.4

--- lib/sendgrid_actionmailer.rb
+++ lib/sendgrid_actionmailer.rb
@@ -123,22 +123,22 @@ module SendGridActionMailer
          sendgrid_mail.template_id = mail['template_id'].to_s
       end
       if mail['sections']
-        mail['sections'].instance_variable_get(:@unparsed_value).each do |key, value|
+        JSON.parse(mail['sections'].value.gsub(/=>/, ":")).each do |key, value|
           sendgrid_mail.add_section(Section.new(key: key, value: value))
         end
       end
       if mail['headers']
-        mail['headers'].instance_variable_get(:@unparsed_value).each do |key, value|
+        JSON.parse(mail['headers'].value.gsub(/=>/, ":")).each do |key, value|
           sendgrid_mail.add_header(Header.new(key: key, value: value))
         end
       end
       if mail['categories']
-        mail['categories'].instance_variable_get(:@unparsed_value).each do |value|
-          sendgrid_mail.add_category(Category.new(name: value))
+        mail['categories'].value.split(",").each do |value|
+          sendgrid_mail.add_category(Category.new(name: value.strip))
         end
       end
       if mail['custom_args']
-        mail['custom_args'].instance_variable_get(:@unparsed_value).each do |key, value|
+        JSON.parse(mail['custom_args'].value.gsub(/=>/, ":")).each do |key, value|
           sendgrid_mail.add_custom_arg(CustomArg.new(key: key, value: value))
         end
       end
@@ -147,7 +147,7 @@ module SendGridActionMailer
         sendgrid_mail.batch_id= mail['batch_id'].to_s
       end
       if mail['asm']
-        asm = JSON.parse(JSON[mail['asm'].instance_variable_get(:@unparsed_value)], symbolize_names: true)
+        asm = JSON.parse(mail['asm'].value.gsub(/=>/, ":"), symbolize_names: true)
         asm =  asm.delete_if { |key, value| !key.to_s.match(/(group_id)|(groups_to_display)/) }
         if asm[:group_id]
           sendgrid_mail.asm = ASM.new(asm)
@@ -160,7 +160,7 @@ module SendGridActionMailer
 
     def add_mail_settings(sendgrid_mail, mail)
       if mail['mail_settings']
-        settings = JSON.parse(JSON[mail['mail_settings'].instance_variable_get(:@unparsed_value)], symbolize_names: true)
+        settings = JSON.parse(mail['mail_settings'].value.gsub(/=>/, ":"), symbolize_names: true)
         sendgrid_mail.mail_settings = MailSettings.new.tap do |m|
           if settings[:bcc]
             m.bcc = BccSettings.new(settings[:bcc])
@@ -183,7 +183,7 @@ module SendGridActionMailer
 
     def add_tracking_settings(sendgrid_mail, mail)
       if mail['tracking_settings']
-        settings = JSON.parse(JSON[mail['tracking_settings'].instance_variable_get(:@unparsed_value)], symbolize_names: true)
+        settings = JSON.parse(mail['tracking_settings'].value.gsub(/=>/, ":"), symbolize_names: true)
         sendgrid_mail.tracking_settings = TrackingSettings.new.tap do |t|
           if settings[:click_tracking]
             t.click_tracking = ClickTracking.new(settings[:click_tracking])
