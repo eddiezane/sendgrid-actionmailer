4431837c69e930280e30ea7cfb8e76e775c3987c
Ty Rauber <tyrauber@mac.com>
2020-01-21 11:05:31 +0100

    Revert "Lazy load ActionMailer::Base" (#64)
    * Revert "Lazy load ActionMailer::Base (#57)"
    
    This reverts commit 356aad107fd54136f6e549a927dc8f5159407b0b.
    
    * Bump to 4.2.2

--- lib/sendgrid_actionmailer/railtie.rb
+++ lib/sendgrid_actionmailer/railtie.rb
@@ -1,9 +1,7 @@
 module SendGridActionMailer
   class Railtie < Rails::Railtie
-    initializer 'sendgrid_actionmailer.add_delivery_method' do
-      ActiveSupport.on_load(:action_mailer) do
-        ActionMailer::Base.add_delivery_method(:sendgrid_actionmailer, SendGridActionMailer::DeliveryMethod)
-      end
+    initializer 'sendgrid_actionmailer.add_delivery_method', before: 'action_mailer.set_configs' do
+      ActionMailer::Base.add_delivery_method(:sendgrid_actionmailer, SendGridActionMailer::DeliveryMethod)
     end
   end
 end
